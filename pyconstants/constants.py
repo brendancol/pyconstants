6c463e3b230d0cd287b1cdfa150e2be0613226fc
bcollins <bcollins@continuum.io>
2016-06-29 11:10:35 -0400

    added code

---
+++ pyconstants/constants.py
@@ -0,0 +1,13 @@
+# constants.py
+
+import numpy
+
+E = 2.718
+PI = 3.14159
+
+def compute_pi():
+    return 4.0*numpy.arctan(1.0)
+
+def print_const(x):
+    print("You constant is", x)
+    return None
