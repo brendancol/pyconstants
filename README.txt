0ec3653a3f2d1e855cb3b230914824afd8f72194
bcollins <bcollins@continuum.io>
2016-06-29 11:08:25 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a sample package for learning to use conda build
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='pyconstants',
+    version='0.0.1',
+    license='CC BY-NC-SA 4.0',
+    long_description=open('README.txt').read(),
+    packages=['pyconstants',],
+)
